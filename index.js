5bbdb17ded53c46309d5c9a3c96c9e6151edbfcf
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-29 12:02:10 +0600

    feat: Services get added

--- index.js
+++ index.js
@@ -10,30 +10,35 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ffrq.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
 async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // await client.close();
-  }
+    try {
+        await client.connect();
+        const carDoctorsCollection = client.db('carDoctorsDB').collection('carDoctors');
+        app.get('/services', async (req, res) => {
+            const result = await carDoctorsCollection.find().toArray();
+            res.send(result);
+        })
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Car-Doctor Resources Running')
+    res.send('Car-Doctor Resources Running')
 })
 
 app.listen(port, () => {
-  console.log(`Car-Doctor Resources Running ${port}`)
+    console.log(`Car-Doctor Resources Running ${port}`)
 })
