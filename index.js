e9a9d32fb1d13a8857c5f744c92bce5bf9de0721
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-29 23:52:13 +0600

    feat: Services post added

--- index.js
+++ index.js
@@ -32,6 +32,36 @@ async function run() {
             const result = await carDoctorsCollection.findOne(query);
             res.send(result);
         })
+        app.post('/services', async (req, res) => {
+            const data = req.body;
+            const doc = {
+                title: data.name,
+                img: data.photo,
+                service_id: data.serviceID,
+                price: data.price,
+                description: data.description,
+                facility: [
+                    {
+                        name: data.facilities[0].fName1,
+                        details: data.facilities[0].fDetails1
+                    },
+                    {
+                        name: data.facilities[1].fName2,
+                        details: data.facilities[1].fDetails2
+                    },
+                    {
+                        name: data.facilities[2].fName3,
+                        details: data.facilities[2].fDetails3
+                    },
+                    {
+                        name: data.facilities[3].fName4,
+                        details: data.facilities[3].fDetails4
+                    }
+                ]
+            }
+            const result = await carDoctorsCollection.insertOne(doc);
+            res.send(result);
+        })
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
